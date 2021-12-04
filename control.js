38571ba803e517661a079205bac1c6ea6bac7c0d
Sidiale1 <95510075+Sidiale1@users.noreply.github.com>
2021-12-03 21:47:58 -0300

    Add files via upload

---
+++ control.js
@@ -0,0 +1,48 @@
+function mudar(){
+  for(i=1;i<11;i++){
+    alert(i+" mudança(s)");
+  }
+document.getElementById("result").innerHTML= "Mudou!"
+}
+
+function ligada1(){
+  document.getElementById("Luz1").src='https://i.postimg.cc/LsxPPkyc/Lon.png'
+}
+function desligada1(){
+  document.getElementById("Luz1").src='https://i.postimg.cc/sXcrDMRZ/Loff.png'
+}
+
+function ligada2(){
+  document.getElementById("Luz2").src='https://i.postimg.cc/LsxPPkyc/Lon.png'
+}
+function desligada2(){
+  document.getElementById("Luz2").src='https://i.postimg.cc/sXcrDMRZ/Loff.png'
+}
+
+function ligada3(){
+  document.getElementById("Luz3").src='https://i.postimg.cc/LsxPPkyc/Lon.png'
+}
+function desligada3(){
+  document.getElementById("Luz3").src='https://i.postimg.cc/sXcrDMRZ/Loff.png'
+}
+
+function ligada4(){
+  document.getElementById("Luz4").src='https://i.postimg.cc/LsxPPkyc/Lon.png'
+}
+function desligada4(){
+  document.getElementById("Luz4").src='https://i.postimg.cc/sXcrDMRZ/Loff.png'
+}
+
+function ligada5(){
+  document.getElementById("Luz5").src='https://i.postimg.cc/LsxPPkyc/Lon.png'
+}
+function desligada5(){
+  document.getElementById("Luz5").src='https://i.postimg.cc/sXcrDMRZ/Loff.png'
+}
+
+function ligada6(){
+  document.getElementById("Luz6").src='https://i.postimg.cc/LsxPPkyc/Lon.png'
+}
+function desligada6(){
+  document.getElementById("Luz6").src='https://i.postimg.cc/sXcrDMRZ/Loff.png'
+}
